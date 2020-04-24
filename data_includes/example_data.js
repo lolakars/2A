7b4034448d92400f737f462d595652cd0a47a63f
lolakars <lolakars@gmail.com>
2020-04-24 23:23:03 +0300

    Create example_data.js

---
+++ data_includes/example_data.js
@@ -0,0 +1,43 @@
+<div style = "width: 40em;">
+<font face = "Arial"> <p> Below, you will see a series of sentences that you will have to score depending on how acceptable they seem to you. To do this, you will have to use a scale from 1 (not acceptable at all) to 7 (totally acceptable) </p>
+
+<p> A sentence is totally acceptable when hearing it you can easily understand it and it sounds natural to you, that is, a native speaker could use it in a conversation. A sentence is totally unacceptable when it doesn't make sense and / or doesn't sound natural to you, that is, a native speaker would never say something like that. Therefore, do not think about the grammar rules that you could have learned in school or institute, be guided by your intuitions. </p>
+
+<p> Before starting the experiment, please answer the following questions: </p>
+
+
+<strong> 1. Were you born and raised in Spain? </strong> </br>
+    Yes
+    <input name = "spain" type = "radio" class = "obligatory" value = "yes" />
+    No
+    <input name = "spain" type = "radio" class = "obligatory" value = "no" />
+
+</br>
+</br>
+
+<strong> 2. Does your family always speak Spanish at home? </strong> </br>
+    Yes
+    <input name = "spanish" type = "radio" class = "obligatory" value = "yes" />
+    No
+    <input name = "spanish" type = "radio" class = "obligatory" value = "no" />
+
+</br>
+</br>
+
+<strong> 3. Age: </strong>
+    <input name = "age" type = "text" size = "2" class = "obligatory" />
+
+</br>
+</br>   
+
+<strong> 4. Sex: </strong>
+    <input name = "sex" type = "radio" value = "male" class = "obligatory" /> Male
+    <input name = "sex" type = "radio" value = "female" class = "obligatory" /> Woman
+    <input name = "sex" type = "radio" value = "nonbinary" class = "obligatory" /> Non-binary
+
+</br>
+</br> 
+
+<input type = "checkbox" class = "obligatory" name = "consent" /> I agree to participate in the experiment
+
+</font>
